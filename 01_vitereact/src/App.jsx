8fb35c0eaa2c06b9399606cf34bfc8ae521439ec
Vikrant <06learntocode11@gmial.com>
2024-01-09 21:25:27 +0530

    create through react element

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Chai from './chai.jsx'
 
 function App() {
-
+// How to inject variables/js in the HTML 
+const username = 'Vikrant'
   return (
     <>
       <Chai/>
-      <h2>chai with vite</h2>
+      <h2>chai with vite with {username}</h2>
       <p>test the Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum cupiditate consequuntur distinctio eaque vitae, rem deserunt unde eum fuga ipsa, magni vel! Itaque?</p>
     </>
   )
--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -2,8 +2,54 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+// function MyApp() {
+//   return (
+//     <div>
+//       <h2>welcome to chai aur react series</h2>
+//     </div>
+//   )
+// }
+
+// Custom react component
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://react.dev',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit react website'
+// }
+
+// const anotherElement = (
+//   <a href="https://react.dev" target='_blank'></a>
+// )
+
+const userName = 'chai aur react'
+ const reactElement = React.createElement(
+  // 1st param: HTML tag
+  'a',
+  // 2nd param: add props like href
+  {
+    href: 'https://react.dev',
+    target: '_blank'
+  },
+  // 3rd param: add text
+  'click me to visit react website',
+
+  // parsing the user name param
+  userName
+ )
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+
+  // *************OR********* //
+
+  // MyApp()
+  // anotherElement
+  reactElement
 )
