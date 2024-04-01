64a85b4d4c358cc7d48778536d9edb13761ebc10
Vikrant <06learntocode11@gmial.com>
2024-04-01 14:02:06 +0530

    TODO: checking authStatus

--- 13_megaBlogAppwrite/src/components/AuthLayout.jsx
+++ 13_megaBlogAppwrite/src/components/AuthLayout.jsx
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from 'react'
-import { useSelecotr } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 
 export const Protected = ({ children, authentication = true}) => {
+
     const [loader, setLoader] = useState(true);
     const navigate = useNavigate();
-    const authStatus = useSelecotr(state => state.auth.status);
+    const authStatus = useSelector(state => state.auth.status);
     
     useEffect(() => {
+        // TODO:
+        // {authStatus ? navigate('/') : navigate('/login')}
+        
         if (authentication && authStatus !== authentication) {
             navigate('/login');
         } else if (!authentication && authStatus !== authentication) {
             navigate('/');
         }
+        setLoader(false);
     }, [authStatus, navigate, authentication])
     
   return loader ? <h1>Loading...</h1> : <>{children}</>
