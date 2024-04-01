364c15271f27fc3f07bf5bee36b79e55d977bf06
Vikrant <06learntocode11@gmial.com>
2024-04-01 14:22:00 +0530

    refactor: code

--- 13_megaBlogAppwrite/src/components/header/LogoutBtn.jsx
+++ 13_megaBlogAppwrite/src/components/header/LogoutBtn.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import authService from '../../store/authSlice'
-import { logout } from '../../store/authSlice'
-
+import authService from '../../appwrite/auth'
+import logout from '../../store/authSlice'
 const LogoutBtn = () => {
     const dispatch = useDispatch();
     const logoutHandler = () => {
         authService.logout().then(() => {
-            dispatch(logout());
+          dispatch(logout())
         })
     }
     
   return (
-    <Button className='inline-block px-6 py-2 duration-200 hover:bg-blue-200 rounded-full'>
+    <button 
+      className='inline-block px-6 py-2 
+                duration-200 hover:bg-blue-200 
+                rounded-full'
+      onClick={logoutHandler}
+    >
         Logout
-    </Button>
+    </button>
   )
 }
 
